bfa127a3737f80655e149a4411d2c452f60cc6db
abhi-alphanimble <abhishek.kundagol@alphanimble.com>
2025-11-06 14:49:18 +0530

    first

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,17 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import { Tabs } from "expo-router";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+  <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
+    
+    <Tabs.Screen name="index" options={{ title: "Home", tabBarIcon: ({color, focused}) => {
+      return focused ? (<Entypo name="home" size={24} color={color}/>) : (<AntDesign name="home" size={24} color={color} />);
+    },
+    }} 
+    />
+    <Tabs.Screen name="login" options={{ title: "Login" }} />
+  </Tabs> 
   );
 }
